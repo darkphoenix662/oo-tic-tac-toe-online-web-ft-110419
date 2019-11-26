f2b0b7c93c55cd2e4a9738631fdcbbae65167432
Sean Good <tree662@protonmail.com>
2019-11-26 20:27:33 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,37 @@
+class TicTacToe
+  attr_accessor :board, :WIN_COMBINATIONS
+  
+   WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+      ]
+  
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(string)
+    num = string.to_i                #stackoverflow
+    num if num.to_s == string        #stackoverflow
+    
+    
+  end 
+  
+  
+  
+end
